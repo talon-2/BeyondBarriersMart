7c9c6812058c0de3ae11764d27dc25cc51a63c03
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2023-04-23 20:31:05 +0800

    tried Charts
    i tried to do charts in fragments but i cant i spent like 3 hours trying to figure this out i've looked like 10 tutorials and cant display any data what am i doing wrong KDBIFVIBISIDUBCIURBIIBICUSIAAAAAA

--- app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
@@ -1,8 +1,14 @@
 package my.edu.tarc.beyondbarriersmart
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.ui.AppBarConfiguration
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/my/edu/tarc/beyondbarriersmart/SpendingChartFragment.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/SpendingChartFragment.kt
@@ -1,59 +1,61 @@
 package my.edu.tarc.beyondbarriersmart
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SpendingChartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SpendingChartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var barList: ArrayList<BarEntry> //create arraylist for data
+    lateinit var barDataSet : BarDataSet
+    lateinit var barData: BarData
+    lateinit var barChart: BarChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spending_chart, container, false)
+        val view = inflater.inflate(R.layout.fragment_spending_chart, container, false)
+        barChart = view.findViewById(R.id.barchart)
+
+        setBarData()
+
+        barDataSet = BarDataSet(barList, "Bar Chart Data")
+        barData = BarData(barDataSet)
+        barChart.data = barData
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.color = Color.BLUE
+        //barDataSet.setColor(resources.getColor(R.color.purple_200))
+
+        barDataSet.valueTextSize = 16f
+        barChart.description.isEnabled = false
+        //barChart.visibility = View.VISIBLE
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpendingChartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SpendingChartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+     private fun setBarData(){
+        barList = ArrayList()
+        barList.add(BarEntry(1f, 1f))
+        barList.add(BarEntry(2f, 2f))
+        barList.add(BarEntry(3f, 3f))
+        barList.add(BarEntry(4f, 4f))
+
+
     }
+
 }
