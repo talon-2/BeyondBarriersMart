0a4687c42d953c860018ebedb1ab1b06092c7140
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2023-04-23 14:59:05 +0800

    added a textbox design and setup addproducts Page

--- app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
@@ -2,10 +2,12 @@ package my.edu.tarc.beyondbarriersmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_home_page)
+        setContentView(R.layout.fragment_product)
     }
 }
