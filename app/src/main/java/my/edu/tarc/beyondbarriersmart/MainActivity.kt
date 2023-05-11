77cb598f169234481045ce0daedc1d385884e7ed
jim-low <robil12380@gmail.com>
2023-05-11 18:52:40 +0800

    feat: add buyer profile assets

--- app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
@@ -18,50 +18,22 @@ class MainActivity : AppCompatActivity() {
     val db = Firebase.firestore
     private lateinit var bottomNavigationView: BottomNavigationView
 
-    var isSellerRegistration = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.fragment_add_products)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_main)
 
-        registrationThings()
-
-//        val testFragment = SellerRegistrationFragment()
+        val testFragment = BuyerProfileFragment()
 
 //         Add BottomNavigationFragment
-//        val bottomNavigationFragment = BottomNavFragment()
+        val bottomNavigationFragment = BottomNavFragment()
 
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.fragment_container, testFragment)
-//            .add(R.id.fragment_container, bottomNavigationFragment)
-//            .commit()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, testFragment)
+            .add(R.id.fragment_container, bottomNavigationFragment)
+            .commit()
 
         //starting a activity (to be placed in an if else statement.)
         //some Activities can only be seen if you run it instead of looking it from the Design preview.
         //startActivity(Intent(this,MyProductsActivity::class.java))
-    }
-
-    private fun registrationThings() {
-        val registerAsBuyerOrSellerText: TextView = findViewById(R.id.register_seller_or_buyer_clickable_text)
-        val sellerFragment = SellerRegistrationFragment()
-        val buyerFragment = BuyerRegistrationFragment()
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.register_form_fragment_layout, buyerFragment)
-            .commit()
-
-        registerAsBuyerOrSellerText.setOnClickListener {
-            isSellerRegistration = !isSellerRegistration
-
-            if (isSellerRegistration) {
-                registerAsBuyerOrSellerText.setText(getString(R.string.register_buyer))
-                supportFragmentManager.beginTransaction().replace(R.id.register_form_fragment_layout, sellerFragment).commit()
-            }
-            else {
-                registerAsBuyerOrSellerText.setText(getString(R.string.register_seller))
-                supportFragmentManager.beginTransaction().replace(R.id.register_form_fragment_layout, buyerFragment).commit()
-            }
-        }
     }
 }
