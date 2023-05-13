f5270276dd019e2fdcfa2c5660bcf17b83da63ff
jim-low <robil12380@gmail.com>
2023-05-14 00:13:03 +0800

    feat: add text over image for login and register, add some code that dont work but at least there is some code

--- app/src/main/java/my/edu/tarc/beyondbarriersmart/LoginFragment.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/LoginFragment.kt
@@ -5,55 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val email_input: EditText? = getView()?.findViewById(R.id.login_username_edit)
+    private val password_input: EditText? = getView()?.findViewById(R.id.login_password_edit)
+    private val forgot_password_clickable_text: TextView? = getView()?.findViewById(R.id.forgot_password_clickable_text)
+    private val create_new_account_clickable_text: TextView? = getView()?.findViewById(R.id.create_account_clickable_text)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    private fun validateLogin() {
+        val email: String = email_input?.text.toString()
+        // what is love, baby dont hurt me
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun forgotPassword() {
+
+    }
+
+    private fun createAccount() {
+
     }
 }
--- app/src/main/java/my/edu/tarc/beyondbarriersmart/RegisterActivity.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/RegisterActivity.kt
@@ -3,28 +3,65 @@ package my.edu.tarc.beyondbarriersmart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+    private var isSellerRegistration = true
+
+    // general components
+    private val sellerFragment = SellerRegistrationFragment()
+    private val buyerFragment = BuyerRegistrationFragment()
+    private val title_placeholder: TextView = findViewById(R.id.welcome_registration_text)
+    private val short_description_placeholder: TextView = findViewById(R.id.welcome_short_desc_text)
+    private val register_as_clickable_text: TextView = findViewById(R.id.register_seller_or_buyer_clickable_text)
+    private val register_button: Button = findViewById(R.id.register_button)
+
+    // form components
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("RegisterActivity", "onCreate has been run in RegisterActivity")
-//        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_register)
 
-//        val registerFragment = SellerRegistrationFragment()
-//
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.register_form_fragment_layout, registerFragment)
-//            .commit()
+        register_as_clickable_text.setOnClickListener {
+            isSellerRegistration = !isSellerRegistration
+            switchRegistration()
+        }
+
+        register_button.setOnClickListener {
+            checkRegisterValidation()
+            register()
+        }
+
+        switchRegistration()
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun checkRegisterValidation() {
 
-        Log.d("RegisterActivity", "onStart has been run in RegisterActivity")
-        val registerFragment = SellerRegistrationFragment()
+    }
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.register_form_fragment_layout, registerFragment)
-            .commit()
+    private fun register() {
+
+    }
+
+    private fun switchRegistration() {
+        if (isSellerRegistration) {
+            title_placeholder.setText(R.string.welcome_seller)
+            short_description_placeholder.setText(R.string.seller_description)
+            register_as_clickable_text.setText(R.string.register_buyer)
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.register_form_fragment_layout, sellerFragment)
+                .commit()
+        }
+        else {
+            title_placeholder.setText(R.string.welcome_user)
+            short_description_placeholder.setText(R.string.user_description)
+            register_as_clickable_text.setText(R.string.register_buyer)
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.register_form_fragment_layout, buyerFragment)
+                .commit()
+        }
     }
 }
