9eed7fc9a20401e3753b78f13918c9c4299c9223
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2023-05-13 22:07:05 +0800

    bug fixing

--- app/src/main/java/my/edu/tarc/beyondbarriersmart/MyProductsActivity.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/MyProductsActivity.kt
@@ -19,6 +19,7 @@ class MyProductsActivity : AppCompatActivity() {
     private val noteRef = db.collection("SellerProductItem")
     private val myObjects = mutableListOf<SellerProductItem>()
     private lateinit var addButton : Button
+    private lateinit var refreshButton: Button
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: SellerProductCardAdapter
@@ -29,9 +30,9 @@ class MyProductsActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         readData() //prayge
-        dummydata() //to be removed once database is implemented
 
         addButton = findViewById(R.id.listAddButton)
+        refreshButton = findViewById(R.id.myProductRefreshButton)
 
         val sellerProductItem = this
         binding.sellerProductsRecyclerView.apply{
@@ -47,99 +48,64 @@ class MyProductsActivity : AppCompatActivity() {
         addButton.setOnClickListener(){
             val intent = Intent(this, AddProductsActivity::class.java)
             startActivity(intent)
+        }
+
+        refreshButton.setOnClickListener(){
+            //refresh the current activity.
             finish()
+            startActivity(getIntent())
         }
     }
 
 
 
     private fun readData(){
+        //create a temporary list to store data in case data gets duplicated.
         val tempProductList = mutableListOf<SellerProductItem>()
+
+        //current seller data (TO BE UPDATED)
+        val currentSellerId = "S0001"
+
+        //initiate coroutine to read data
         noteRef.get().addOnCompleteListener { task ->
             if(task.isSuccessful){
-
                 val querySnapshot = task.result
-
                 for (document in querySnapshot?.documents ?: emptyList()) {
-
                 val data = document.data
 
-                val item = SellerProductItem(
-                    "${data?.get("productId")}",
-                    "${data?.get("sellerId")}",
-                    "${data?.get("image")}",
-                    "${data?.get("name")}",
-                    "${data?.get("description")}",
-                    "${data?.get("category")}",
-                    "${data?.get("stock")}".toInt(),
-                    "${data?.get("sold")}".toInt(),
-                    "${data?.get("rating")}".toFloat(),
-                    "${data?.get("cost")}".toFloat(),
-                    "${data?.get("shipNeed")}".toBoolean()
-                )
-                productList.add(item)
-                tempProductList.add(item)
+                //if the item belongs to the current Seller, store it.
+                    if(currentSellerId == "${data?.get("sellerId")}"){
+                        val item = SellerProductItem(
+                            "${data?.get("productId")}",
+                            "${data?.get("sellerId")}",
+                            "${data?.get("image")}",
+                            "${data?.get("name")}",
+                            "${data?.get("description")}",
+                            "${data?.get("category")}",
+                            "${data?.get("stock")}".toInt(),
+                            "${data?.get("sold")}".toInt(),
+                            "${data?.get("rating")}".toFloat(),
+                            "${data?.get("cost")}".toFloat(),
+                            "${data?.get("shipNeed")}".toBoolean()
+                        )
+                        productList.add(item)
+                        tempProductList.add(item)
+                    }
+
+                        val adapter = SellerProductCardAdapter(productList)
+                        val recyclerView = findViewById<RecyclerView>(R.id.sellerProductsRecyclerView)
+                        recyclerView.adapter = adapter
+                        recyclerView.layoutManager = LinearLayoutManager(this)
+                        productList.clear()
+                        productList.addAll(tempProductList)
+                        adapter.notifyDataSetChanged()
                 }
 
-                val adapter = SellerProductCardAdapter(productList)
-                val recyclerView = findViewById<RecyclerView>(R.id.sellerProductsRecyclerView)
-                recyclerView.adapter = adapter
-                recyclerView.layoutManager = LinearLayoutManager(this)
-                productList.clear()
-                productList.addAll(tempProductList)
-                adapter.notifyDataSetChanged()
 
             }
         }.addOnFailureListener { exception ->
             Log.w(TAG, "There is an error with reading the database (Source: MyProductsActivity).", exception)
         }
 
-    }
-
-    private fun dummydata() {
-//        val item1 = SellerProductItem(
-//            myObjects.elementAt(0).productId,
-//            "tempSeller1",
-//            R.drawable.baseline_person_24,
-//            "Prod1",
-//            "desc1",
-//            "Fashion",
-//            10,
-//            10,
-//            5f,
-//            10f,
-//            false
-//        )
-//        productList.add(item1)
-//
-//        val item2 = SellerProductItem(
-//            "productID1",
-//            "tempSeller1",
-//            R.drawable.baseline_person_24,
-//            "Prod1",
-//            "desc1",
-//            "Fashion",
-//            10,
-//            10,
-//            5f,
-//            10f,
-//            false
-//        )
-//        productList.add(item2)
-//
-//        val item3 = SellerProductItem(
-//            "productID1",
-//            "tempSeller1",
-//            R.drawable.baseline_person_24,
-//            "Prod1",
-//            "desc1",
-//            "Fashion",
-//            10,
-//            10,
-//            5f,
-//            10f,
-//            false
-//        )
-//        productList.add(item3)
     }
 }
