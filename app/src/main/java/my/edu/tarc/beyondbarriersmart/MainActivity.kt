8d0630b3f11a0368ff72ca9737adb5e8d53169e6
jim-low <robil12380@gmail.com>
2023-05-15 18:37:05 +0800

    chore: change to login fragment

--- app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/beyondbarriersmart/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val testFragment = CartFragment()
+        val testFragment = LoginFragment()
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container, testFragment)
